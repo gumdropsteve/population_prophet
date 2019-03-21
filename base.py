4b543e7ae689a45a4254450f9a52563f0a4d455c
Winston <winston@Winstons-MacBook-Pro.local>
2019-03-20 18:56:09 -0700

    reflect updated file name

--- base.py
+++ base.py
@@ -1,8 +1,7 @@
-from api-key import api_key
-from census import Census
 from us import states
+from census import Census
+from api_key import api_key
 
-c = Census(api_key)
-x = c.acs5.get(('NAME', 'B25034_010E'),
-          {'for': 'state:{}'.format(states.MD.fips)})
+c = Census( api_key )
+x = c.acs5.get( ( 'NAME' , 'B25034_010E' ) , { 'for': f'state:{states.MD.fips}' } )
 print(x)
